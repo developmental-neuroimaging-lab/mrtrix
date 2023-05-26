e8c3df62e37f8b55daf06d83b3567c698b8b96ba
Meaghan Perdue <meaghan.perdue@ucalgary.ca>
2023-05-26 10:31:14 -0600

    new comment

--- preschool_scripts/data_transfer/rsync_bids2arc.sh
+++ preschool_scripts/data_transfer/rsync_bids2arc.sh
@@ -1,9 +1,10 @@
 # transfer data from Rundle to ARC using rsync
 # transfers dwi data only (excludes func, anat, and perf)
 # batchN.txt should contain a list of subjects sub-NNNNN no session numbers, script will copy all sessions
-# make sure to upate batchN.txt in line 7 to the batch you want to transfer
+# upate batchN.txt in line 7 to the batch you want to transfer
+#update path in line 9 to your user.name
 export PS_BIDS=/Volumes/catherine_team/MRI_Data/2_BIDS_Datasets/preschool
 
-for i in $(cat batchN.txt); do
+for i in $(cat batch3.txt); do
     rsync -av $PS_BIDS/${i} --exclude */func --exclude */anat --exclude */perf meaghan.perdue@arc.ucalgary.ca:/home/meaghan.perdue/preschool_bids
     done
