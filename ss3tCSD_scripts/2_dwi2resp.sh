60df31e506ad47c70ec45ea9246794e1a0598cf8
Meaghan Perdue <meaghan.perdue@ucalgary.ca>
2023-11-16 10:19:22 -0700

    update lmax to 6

--- ss3tCSD_scripts/2_dwi2resp.sh
+++ ss3tCSD_scripts/2_dwi2resp.sh
@@ -3,17 +3,18 @@
 # Prep data for FOD estimation
 # run on ARC using mrtrix3 container
 # by Meaghan Perdue
-# Oct 2023
+# Nov 2023
 
 cd /mrtrix_out/${1}/${2}
 mkdir ss3t_csd
 
 #run response function estimation
 echo "-------- Running response function estimation --------"
-dwi2response dhollander ${1}_${2}_dwi_b2000_preprocessed_1mm.mif \
+dwi2response -lmax 6 -info \
+    dhollander ${1}_${2}_dwi_b2000_preprocessed_1mm.mif \
     ss3t_csd/wm_response.txt ss3t_csd/gm_response.txt ss3t_csd/csf_response.txt \
     -mask dwi_b2000_1mm_bet_mask.nii.gz \
-    -voxels ss3t_csd/voxels.mif  -info
+    -voxels ss3t_csd/voxels.mif -force
 
 #optionally view outputs, check shapes by tissue type (sphere vs. flattened) at each bval
 #shview wm_response.txt
