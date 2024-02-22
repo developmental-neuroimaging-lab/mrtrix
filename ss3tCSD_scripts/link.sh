8e74dbf8d64712f19372b3807b575d95c53079af
Meaghan Perdue <meaghan.perdue@ucalgary.ca>
2024-02-22 12:17:15 -0700

    fix filenames

--- ss3tCSD_scripts/link.sh
+++ ss3tCSD_scripts/link.sh
@@ -4,5 +4,5 @@ export mrtrix_out=/work/lebel_lab/mrtrix/data
 
 cd $mrtrix_out
 
-ln -sr ${1}/${2}/wmfod_norm.mif template/fod_input/${1}_${2}.mif
-ln -sr ${1}/${2}/mask.mif template/mask_input/${1}_${2}_mask.mif
+ln -sr ${1}/${2}/ss3t_csd/wmfod_norm.mif template/fod_input/${1}_${2}.mif
+ln -sr ${1}/${2}/${1}_${2}_dwi_b2000_1mm_bet_mask.nii.gz template/mask_input/${1}_${2}_mask.nii.gz
