4cf7c7f93ba1bd25e35035f648b176f20b8f4c8f
Meaghan Perdue <meaghan.perdue@ucalgary.ca>
2024-02-27 10:34:58 -0700

    fix mask path

--- ss3tCSD_scripts/7_regsubs2template.sh
+++ ss3tCSD_scripts/7_regsubs2template.sh
@@ -5,6 +5,6 @@
 
 
 cd /mrtrix_out/${1}/${2}/ss3t_csd
-mrregister wmfod_norm.mif -mask1 ${1}_${2}_dwi_b2000_1mm_bet_mask.nii.gz /mrtrix_out/template/wmfod_template.mif -nl_warp subject2template_warp.mif template2subject_warp.mif -nthreads 16 
-mrtransform ${1}_${2}_dwi_b2000_1mm_bet_mask.nii.gz -warp subject2template_warp.mif -interp nearest -datatype bit dwi_mask_in_template_space.mif -nthreads 16 
+mrregister wmfod_norm.mif -mask1 ../${1}_${2}_dwi_b2000_1mm_bet_mask.nii.gz /mrtrix_out/template/wmfod_template.mif -nl_warp subject2template_warp.mif template2subject_warp.mif -nthreads 16 
+mrtransform ../${1}_${2}_dwi_b2000_1mm_bet_mask.nii.gz -warp subject2template_warp.mif -interp nearest -datatype bit dwi_mask_in_template_space.mif -nthreads 16 
 
