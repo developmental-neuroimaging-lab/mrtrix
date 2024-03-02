6a747f043a39b687ce7f79194d1505c150745596
Meaghan Perdue <meaghan.perdue@ucalgary.ca>
2024-03-02 11:40:57 -0700

    reduce from 5M to 1M streamlines

--- preschool_scripts/4_tckgen_wholebrain.sh
+++ preschool_scripts/4_tckgen_wholebrain.sh
@@ -7,13 +7,13 @@
 
 #generate whole brain tractogram with 5 million streamlines
 
-tckgen -algorithm Tensor_Prob -select 5000000 -nthreads 16 \
+tckgen -algorithm Tensor_Prob -select 1000000 -nthreads 16 \
     -seed_image /mrtrix_out/${1}/${2}/dwi_b750_1mm_bet_mask.nii.gz \
     -mask /mrtrix_out/${1}/${2}/dwi_b750_1mm_bet_mask.nii.gz \
     /mrtrix_out/${1}/${2}/${1}_${2}_dwi_b750_preprocessed_1mm.mif \
-    /mrtrix_out/${1}/${2}/${1}_${2}_tensor_prob_wholebrain_5mil.tck
+    /mrtrix_out/${1}/${2}/${1}_${2}_tensor_prob_wholebrain_1mil.tck
 
 # downsample to 100k streamlines for viewing in mrview
 tckedit -number 100k -nthreads 16 \
-    /mrtrix_out/${1}/${2}/${1}_${2}_tensor_prob_wholebrain_5mil.tck \
+    /mrtrix_out/${1}/${2}/${1}_${2}_tensor_prob_wholebrain_1mil.tck \
     /mrtrix_out/${1}/${2}/${1}_${2}_tensor_prob_wholebrain_100k.tck
