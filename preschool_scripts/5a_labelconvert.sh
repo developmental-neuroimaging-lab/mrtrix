909f88421d6a25e000c4d8d35e06e68d832e06cd
Meaghan Perdue <meaghan.perdue@ucalgary.ca>
2024-03-05 09:51:53 -0700

    add coregistration

--- preschool_scripts/5a_labelconvert.sh
+++ preschool_scripts/5a_labelconvert.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
-# convert MaCRUISE atlas to MRtrix format
+# register MaCRUISE segmentation to FA map, convert MaCRUISE atlas to MRtrix format
 # running locally
 # by Meaghan Perdue
 # Feb 2024
 
+export bids=/Volumes/BIDS/CL_Preschool/derivatives/mrtrix
 export atlas_in=/Volumes/catherine_team/Trainee_Folders/Long/MaCRUISE_atlas4connectome
 export atlas_out=~/Projects/atlas_for_mrtrix
 
-labelconvert $atlas_in/${3}_Seg_reg.nii \
+mkdir -p $atlas_out/${1}/${2}
+
+# register MaCRUISE segmented T1 to FA map
+flirt -in  $atlas_in/${3}_Seg_a.nii \
+      -ref $bids/${1}/${2}/${1}_${2}_dwi_b750_tensor_fa_1mm.nii.gz \
+      -out $atlas_out/${1}/${2}/${1}_${2}_SEG_reg2FA.nii.gz \
+      -omat $atlas_out/${1}/${2}/${1}_${2}_T1_reg2FA.mat
+
+
+labelconvert $atlas_out/${1}/${2}/${1}_${2}_SEG_reg2FA.nii.gz \
             $atlas_in/MaCRUISE_atlas_MRItrix.txt \
-            $atlas_out/MaCRUISE_atlas_MRItrix.txt \
-            $atlas_out/${1}_${2}_${3}_atlas.mif 
-            
+            $atlas_in/MaCRUISE_atlas_MRItrix.txt \
+            $atlas_out/${1}/${2}/${1}_${2}_${3}_atlas.mif 
